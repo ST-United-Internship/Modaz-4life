2569f391e66a45b03b04859b4270adac73532033
ngocphuocha <phuoc04012000@gmail.com>
2023-03-22 23:28:30 +0700

    SEC-01: Done create header UI

---
+++ js/header/header.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  // hide in default
+  $("#vertical-item-menu").slideUp();
+
+  // show menu
+  $("#btn-menu-bar").click(function (e) {
+    $("#vertical-item-menu").slideToggle();
+    $("#icon-btn").toggleClass("bi bi-x");
+  });
+
+  // Hide sub menu default
+  $(".sub-menu").slideUp();
+
+  $("#shop-item").click(function (e) {
+    // Show sub menu
+    $("#shop-item-sub").slideToggle();
+    // Change icon
+    $("#shop-item-icon-down").toggleClass("fbi bi-chevron-up");
+  });
+
+  $("#page-item").click(function (e) {
+    // Show sub menu
+    $("#page-item-sub").slideToggle();
+    // Change icon
+    $("#page-item-icon-down").toggleClass("fbi bi-chevron-up");
+  });
+
+  $("#blog-item").click(function (e) {
+    // Show sub menu
+    $("#blog-item-sub").slideToggle();
+    // Change icon
+    $("#blog-item-icon-down").toggleClass("fbi bi-chevron-up");
+  });
+});
